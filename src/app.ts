6b23a3a79e6715b4c03bc9fddcc583cc52a5a7f9
developer-mahin <mdmahin1310@gmail.com>
2023-11-22 21:17:23 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-unused-vars */
+import express, { NextFunction, Request, Response } from 'express';
+import createError from 'http-errors';
+import cors from "cors"
+const app = express();
+
+
+// build in middleware
+app.use(express.json())
+app.use(cors())
+
+
+
+
+
+app.get('/', async (req: Request, res: Response) => {
+    res.json({
+        message: 'welcome to the server',
+    });
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    next(
+        createError(404, 'Route not found please send request in a valid route'),
+    );
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.status | 500).json({
+        success: false,
+        message: err.message,
+    });
+});
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+
+
+
+async function server() {
+  try {
+    await mongoose.connect(config.database as string)
+      .then(() => {
+        console.log("database connected successfully")
+      })
+
+    app.listen(config.port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Server running on port http://localhost:${config.port}`);
+    });
+  } catch (error: any) {
+    // eslint-disable-next-line no-console
+    console.log(error.message)
+  }
+
+}
+
+
+// eslint-disable-next-line no-console
+server().catch(err => console.log(err.message))
+
